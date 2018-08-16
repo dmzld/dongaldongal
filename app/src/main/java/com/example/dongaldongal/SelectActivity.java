0ba0606f0b296fea0c4471f456d2b7d8a285329e
LEE YOON SEOK <dmzld94@gmail.com>
2018-08-16 15:47:31 +0900

    logout and user info modification fuction

--- app/src/main/java/com/example/dongaldongal/SelectActivity.java
+++ app/src/main/java/com/example/dongaldongal/SelectActivity.java
@@ -1,10 +1,15 @@
 package com.example.dongaldongal;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import org.w3c.dom.Text;
 
 public class SelectActivity extends AppCompatActivity {
 
@@ -13,6 +18,8 @@ public class SelectActivity extends AppCompatActivity {
     Button btnClubList;
     Button btnFest;
     Button btnBoard;
+    ImageButton btnLogout;
+    TextView textInfo;
     Button btnSetting;
 
     @Override
@@ -23,17 +30,20 @@ public class SelectActivity extends AppCompatActivity {
         Intent intent = getIntent();
         student_id = intent.getStringExtra("id");
 
-        btnBoard = (Button)findViewById(R.id.btnBoard);
-        btnClubList = (Button)findViewById(R.id.btnClubList);
-        btnFest = (Button)findViewById(R.id.btnFest);
-        btnSetting=(Button)findViewById(R.id.btnSetting);
+        btnBoard = (Button) findViewById(R.id.btnBoard);
+        btnClubList = (Button) findViewById(R.id.btnClubList);
+        btnFest = (Button) findViewById(R.id.btnFest);
+        btnLogout = (ImageButton) findViewById(R.id.btnLogout);
+        textInfo = (TextView) findViewById(R.id.textInfo);
+        textInfo.setText(student_id + "님");
+        btnSetting = (Button) findViewById(R.id.btnSetting);
 
         btnBoard.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),BoardActivity.class);
-                intent.putExtra("id",student_id);
+                Intent intent = new Intent(getApplicationContext(), BoardActivity.class);
+                intent.putExtra("id", student_id);
                 startActivity(intent);
             }
 
@@ -42,7 +52,7 @@ public class SelectActivity extends AppCompatActivity {
         btnClubList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),ClubListActivity.class);
+                Intent intent = new Intent(getApplicationContext(), ClubListActivity.class);
                 startActivity(intent);
             }
         });
@@ -51,8 +61,8 @@ public class SelectActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),FestivalActivity.class);
-                intent.putExtra("id",student_id);
+                Intent intent = new Intent(getApplicationContext(), FestivalActivity.class);
+                intent.putExtra("id", student_id);
                 startActivity(intent);
             }
 
@@ -62,12 +72,21 @@ public class SelectActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),SettingActivity.class);
-                intent.putExtra("id",student_id);
+                Intent intent = new Intent(getApplicationContext(), SettingActivity.class);
+                intent.putExtra("id", student_id);
                 startActivity(intent);
             }
 
         });
 
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);//현재 TASK를 비우고 새로운 TASK 생성
+                startActivity(intent);
+            }
+            //로그인 할때 얻어온 데이터 처리를 따로 해야하는가?
+        });
     }
 }
